3a6885f427582824bee66d01777e2b570990ce8d
RomanZorkin <zorkin.roman@gmail.com>
2022-03-06 10:06:53 +0300

    review correct

--- backend/measurements.py
+++ backend/measurements.py
@@ -4,13 +4,13 @@ from uuid import uuid4
 from flask import Flask, jsonify, request
 
 
-measurment_storage = {
+measurement_storage = {
     "e7b3d405ac9a45758109f3daee0adfae": {
         "uid": "e7b3d405ac9a45758109f3daee0adfae",
         "test_uid": "1",
         "name": "7.1.2 Overall Delay in SND DVNB",
         "status": "Done",
-        "descriotion": "GOST 33468-NB Rev.04 \ Hands-free Parameters \ Delay Measurements (DVNB) \ SND Direction",
+        "description": "GOST 33468-NB Rev.04 \ Hands-free Parameters \ Delay Measurements (DVNB) \ SND Direction",
         "time": "06.12.2021 14:57",
     },
     "3074db5ea6064b75b5c8e13d0415a4e3": {
@@ -18,7 +18,7 @@ measurment_storage = {
         "test_uid": "1",
         "name": "7.1.2 Calc: Delay SND DUT GSM DVNB",
         "status": "Ok",
-        "descriotion": "GOST 33468-NB Rev.04 \ Hands-free Parameters \ Delay Measurements (DVNB) \ SND Direction",
+        "description": "GOST 33468-NB Rev.04 \ Hands-free Parameters \ Delay Measurements (DVNB) \ SND Direction",
         "time": "06.12.2021 14:58",
     },
 }
@@ -26,42 +26,42 @@ measurment_storage = {
 app = Flask(__name__)
 
 
-@app.get('/api/measurements/')
+@app.get('/api/v1/measurements/')
 def get_measurements():
-    measurements = [measurment for _, measurment in measurment_storage.items()]
+    measurements = [measurment for _, measurment in measurement_storage.items()]
     return jsonify(measurements)
 
 
-@app.get('/api/measurements/<uid>')
+@app.get('/api/v1/measurements/<uid>')
 def get_by_id(uid):
-    measurment = measurment_storage.get(uid)
+    measurment = measurement_storage.get(uid)
     if not measurment:
         return {'message': 'measurment not found'}, HTTPStatus.NOT_FOUND
     return measurment
 
 
-@app.post('/api/measurements/')
+@app.post('/api/v1/measurements/')
 def add_measurement():
-    measurment = request.json
-    measurment['uid'] = uuid4().hex
-    measurment_storage[measurment['uid']] = measurment
-    return measurment, HTTPStatus.CREATED
+    measurement = request.json
+    measurement['uid'] = uuid4().hex
+    measurement_storage[measurement['uid']] = measurement
+    return measurement, HTTPStatus.CREATED
 
 
-@app.put('/api/measurements/<uid>')
+@app.put('/api/v1/measurements/<uid>')
 def update_measurement(uid):
-    if uid not in measurment_storage:
+    if uid not in measurement_storage:
         return {"message": "measurment not found"}, HTTPStatus.NOT_FOUND
     measurment = request.json
-    measurment_storage[uid] = measurment
+    measurement_storage[uid] = measurment
     return measurment, HTTPStatus.OK
 
 
-@app.delete('/api/measurements/<uid>')
+@app.delete('/api/v1/measurements/<uid>')
 def delete_measurement(uid):
-    if uid not in measurment_storage:
+    if uid not in measurement_storage:
         return {"message": "measurment not found"}, HTTPStatus.NOT_FOUND
-    measurment_storage.pop(uid)
+    measurement_storage.pop(uid)
     return {}, HTTPStatus.NO_CONTENT
 
 
