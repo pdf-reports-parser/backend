94e34379ea96401461c7ffc541b0eb3a7c635f9f
astra <zorkin.roman@gmail.com>
2022-03-13 18:26:35 +0300

    Exceptions added to repo

--- service/repos/measurements.py
+++ service/repos/measurements.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from typing import Optional
 
-import service.errors
+from sqlalchemy.exc import IntegrityError
+
 from service.db import db_session
+from service.errors import ConflictError, NotFoundError
 from service.models import Measurements
 
 
@@ -35,31 +36,33 @@ class MeasurementsRepo:
         description: str,
         measure_time: datetime,
         test_id: int,
-    ) -> Optional[Measurements]:
+    ) -> Measurements:
         measure: Measurements = Measurements.query.filter_by(uid=uid).first()
         if not measure:
-            return None
-        measure.name = name
-        measure.status = status
-        measure.description = description
-        measure.measure_time = measure_time
-        measure.test_id = test_id
-        db_session.commit()
+            raise NotFoundError('measure')
+        try:
+            measure.name = name
+            measure.status = status
+            measure.description = description
+            measure.measure_time = measure_time
+            measure.test_id = test_id
+            db_session.commit()
+        except IntegrityError:
+            raise ConflictError('measure')
         return measure
 
     def get_all(self) -> Measurements:
         return Measurements.query.all()
 
-    def get_by_uid(self, uid: int) -> Optional[Measurements]:
+    def get_by_uid(self, uid: int) -> Measurements:
         measure: Measurements = Measurements.query.filter_by(uid=uid).first()
         if not measure:
-            return None
+            raise NotFoundError('measure')
         return measure
 
-    def delete(self, uid: int) -> bool:
+    def delete(self, uid: int) -> None:
         measure = Measurements.query.filter_by(uid=uid).first()
         if not measure:
-            return False
+            raise NotFoundError('measure')
         db_session.delete(measure)
         db_session.commit()
-        return True
