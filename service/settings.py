8768645b8413edb2a1acf5dcfd58b228d823f5ab
astra <zorkin.roman@gmail.com>
2022-03-16 15:03:27 +0300

    Settings & env correct

--- service/settings.py
+++ service/settings.py
@@ -1,8 +1,13 @@
 import os
+from sys import argv
 
 
-DB_URL: str = os.environ['DB_URL_DOCKER']
+if len(argv) > 2 and argv[2] == '--local':
+    DB_URL: str = os.environ['DB_URL_LOCAL']
+    PORT: str = os.environ['PORT_LOCAL']
 
-PORT: str = os.environ['PORT_DOCKER']
+else:
+    DB_URL: str = os.environ['DB_URL_DOCKER']
+    PORT: str = os.environ['PORT_DOCKER']
 
 APP_HOST: str = os.environ['APP_HOST']
