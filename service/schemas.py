da8671d3d06189ebc4080304cb1e8b9cf68ad051
vinnpavel <37072927+vinnpavel@users.noreply.github.com>
2022-03-25 19:25:07 +0300

    Measurements were renamed to Trials, Tests- to Measurements, Measurements were registered as Blueprint (#19)

--- service/schemas.py
+++ service/schemas.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from pydantic import BaseModel, constr
 
 
-class Measurement(BaseModel):
+class Trial(BaseModel):
     uid: int
     name: constr(min_length=1)  # type: ignore
     status: constr(min_length=1)  # type: ignore
     description: constr(min_length=1)  # type: ignore
-    measure_time: datetime
+    trial_time: datetime
     test_id: int
 
     class Config:
