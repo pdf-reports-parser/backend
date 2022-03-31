521c574e18d3d5fa907c8493e529b89300391780
astra <zorkin.roman@gmail.com>
2022-03-31 17:29:20 +0300

    Add measurement to db rule

--- service/extractor/extractor.py
+++ service/extractor/extractor.py
@@ -4,28 +4,36 @@ from pathlib import Path
 import aquaparser
 
 from service import schemas
+from service.repos.measurements import MeasurementsRepo
 from service.repos.trials import TrialsRepo
 
-repo = TrialsRepo()
+measurement_repo = MeasurementsRepo()
+trial_repo = TrialsRepo()
 
 
-def write_to_bd_title(measurement):
-    title = {
-        'measurement': asdict(measurement.title),
-    }
-    # the stub is necessary until we get the id of the new measurement from the database
-    measure_id = 1
-    return measure_id, title
+def write_to_bd_title(measurement_report):
+    payload = asdict(measurement_report.title)
+    payload['uid'] = -1
+    measurement = schemas.Measurement(**payload)
+    entity = schemas.Measurement(
+        measurement_object=measurement.measurement_object,
+        project=measurement.project,
+        report_date=measurement.report_date,
+        responsible_person=measurement.responsible_person,
+    )
+    new_measurement = schemas.Measurement.from_orm(entity)
+    measure_id = new_measurement.uid
+    return measure_id, new_measurement.dict()
 
 
-def write_to_bd_trials(measurement, measure_id: int):
+def write_to_bd_trials(measurement_report, measure_id: int):
     trials_list = []
-    for toc in measurement.toc:
+    for toc in measurement_report.toc:
         payload = asdict(toc)
         payload['uid'] = -1
         payload['measure_id'] = measure_id
         trial = schemas.Trial(**payload)
-        entity = repo.add(
+        entity = trial_repo.add(
             smd=trial.smd,
             status=trial.status,
             value_description=trial.value_description,
