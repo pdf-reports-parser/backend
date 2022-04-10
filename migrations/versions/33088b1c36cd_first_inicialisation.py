d5e59cfc3a4cbbc8b6d8687cd77448bafff8bfbe
astra <zorkin.roman@gmail.com>
2022-04-10 18:55:11 +0300

    Alembic initialisation

---
+++ migrations/versions/33088b1c36cd_first_inicialisation.py
@@ -0,0 +1,24 @@
+"""first inicialisation
+
+Revision ID: 33088b1c36cd
+Revises: 
+Create Date: 2022-04-10 18:46:44.188225
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33088b1c36cd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
